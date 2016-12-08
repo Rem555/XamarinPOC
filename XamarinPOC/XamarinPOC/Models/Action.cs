5d53dfd001d5675bec1b70a37d0283437e7b8c52
Yan <yan.ostashko@gmail.com>
2016-12-08 12:34:46 +0300

    repair driod project reference

---
+++ XamarinPOC/XamarinPOC/Models/Action.cs
@@ -0,0 +1,12 @@
+﻿using PropertyChanged;
+
+namespace XamarinPOC.Models
+{
+    [ImplementPropertyChanged]
+    public class Action
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
